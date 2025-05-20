c6fcb7561554f91ace96801b0c5fd996a17073ec
Yanis Husser <yanis.husser2@etu.univ-lorraine.fr>
2025-05-20 09:18:40 +0200

    ajout des classes Arc et Arcs

--- SAEAlgo/src/Arc.java
+++ SAEAlgo/src/Arc.java
@@ -1,2 +1,17 @@
 public class Arc {
+    private String destination;
+    private int cout;
+
+    public Arc(String dest, int cout){
+        this.destination = dest;
+        this.cout = cout;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public int getCout() {
+        return cout;
+    }
 }
--- SAEAlgo/src/Arcs.java
+++ SAEAlgo/src/Arcs.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Arcs {
+    private ArrayList<Arc> arcs;
+
+    public Arcs() {
+        this.arcs = new ArrayList<>();
+    }
+
+    public void ajouterArc(Arc arc) {
+        this.arcs.add(arc);
+    }
+
+    public ArrayList<Arc> getArcs() {
+        return this.arcs;
+    }
 }
